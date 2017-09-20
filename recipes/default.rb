e34f042e342acc7ae57bedaaf6d83479b55cbcb4
Divesh Mavji <divesh987@hotmail.co.uk>
2017-09-20 10:07:52 +0100

    new version

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,19 +4,59 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-package "mongodb"
+# apt_repository 'mongodb' do
+#   uri 'http://ppa.launchpad.net/juju/stable/ubuntu'
+#   components ['main']
+#   distribution 'trusty'
+#   key 'C8068B11'
+#   keyserver 'keyserver.ubuntu.com'
+#   action :add
+#   deb_src true
+# end
 
-service 'mongodb' do 
-	supports status: true, restart: true
-	action [:enable, :start]
-end
+execute 'add key' do
+    command 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6'
+    action :nothing
+  end
+  execute 'add repo' do
+    command 'sudo echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list'
+    action :nothing
+  end
+  execute 'install mongodb' do
+    command 'sudo apt-get install -y mongodb-org'
+    action :nothing
+  end
 
+  # package "mongodb"
+  
 template '/etc/mongod.conf' do 
 	source 'mongod.conf.erb'
-	notifies :reload, "service[mongodb]"
 end 
 
 template '/lib/systemd/system/mongod.service' do 
 	source 'mongod.service.erb'
+	action :create
 end 
+
+
+execute 'systemctl daemon-reload' do
+    command 'systemctl daemon-reload'
+    action :nothing
+  end
+  execute 'systemctl start mongod' do
+    command 'systemctl start mongod'
+    action :nothing
+  end
+  execute 'systemctl enable mongod' do
+    command 'systemctl enable mongod'
+    action :nothing
+  end
+
+# service 'mongod' do 
+# 	supports status: true, restart: true
+# 	action [:enable, :start]
+# end
+
+
+
 
